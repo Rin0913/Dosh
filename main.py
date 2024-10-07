6312e8188e1fd441f6ec57c6b862ec8e5a846795
K Rin <rin@sandb0x.tw>
2024-10-07 01:42:58 +0000

    Add pylint and improve the code quality.

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
-import subprocess
-import threading
-import sys
-import readline
-
-def attach(target):
-    process = subprocess.Popen(
-        [target],
-        stdin=sys.stdin,
-        stdout=sys.stdout,
-        stderr=sys.stderr,
-        text=True,
-        bufsize=1
-    )
-
-    process.wait()
-
-commands = {
-    "help": (lambda: print("\n".join([commands[command][1] for command in commands])), "help: Show this message."),
-    "exit": (lambda: print("Goodbye!") or exit(0), "exit: Exit Dosh."),
-    "attach": (lambda target: attach(target), "attach <target>: Attach to a container.\n\ttarget: The container you would like to attach."),
-}
+import readline # pylint: disable=unused-import
+from commands import command_list
 
 while True:
-    command = input("dosh> ").split()
-    if not command: continue
-    if command[0] in commands:
-        method = commands[command[0]]
+    try:
+        command = input("dosh> ").split()
+        if not command:
+            continue
+    except KeyboardInterrupt:
+        print()
+        continue
+    except EOFError:
+        command = ["exit"]
+
+    if command[0] in command_list:
+        method = command_list[command[0]]
         try:
             method[0](*command[1:])
         except TypeError:
